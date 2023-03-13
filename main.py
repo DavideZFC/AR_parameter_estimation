6ab71ca24de947e414577acff37ab58d2d17df32
DavideZFC <davide.maran@mail.polimi.it>
2023-03-13 14:52:28 +0100

    names changed

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from functions.LS_estimator import fit_AR
 from classes.make_experiment import Experiment
 import matplotlib.pyplot as plt
 
-
+# Instantiate AR process
 ar = AutoregressiveProcess([0.2, 0.5, -0.1], gamma0 = 1)
 
+# Create class for the experiment and make it
 exp = Experiment(ar, fit_AR)
 exp.make_experiment(seeds = 10, n = 100)
 
+# Create plot
 fig, ax = plt.subplots(figsize=(12, 8))
 exp.plot_confidence_region(ax, col = 'C0', label='confidence regions')
-fig.savefig('figures/data1.pdf')
+
+save_name = 'learning_curve'
+fig.savefig('figures/'+save_name+'.pdf')
